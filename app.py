57763778bb6f7d9d3daab0af3903893386200b7e
Andrew <andrewm1246@gmail.com>
2021-12-03 12:00:32 -0500

    Added dashboard template

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'test'
+# Path for our main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
 
-if __name__ == '__main__':
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 100))
+
+if __name__ == "__main__":
     app.run(debug=True)
