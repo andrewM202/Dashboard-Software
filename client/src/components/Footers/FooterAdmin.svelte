6567c45e82c933ea6754a13b41511706dc71f35e
Andrew <andrewm1246@gmail.com>
2021-12-26 17:01:52 -0500

    Change links in adminfooter to go nowhere

--- client/src/components/Footers/FooterAdmin.svelte
+++ client/src/components/Footers/FooterAdmin.svelte
@@ -13,7 +13,7 @@
         >
           Copyright © {date}
           <a
-            href="https://www.creative-tim.com?ref=ns-footer-admin"
+            href=""
             class="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
           >
             Filluis
@@ -24,7 +24,7 @@
         <ul class="flex flex-wrap list-none md:justify-end justify-center">
           <li>
             <a
-              href="https://www.creative-tim.com?ref=ns-footer-admin"
+              href=""
               class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
             >
               Filluis
@@ -32,7 +32,7 @@
           </li>
           <li>
             <a
-              href="https://www.creative-tim.com/presentation?ref=ns-footer-admin"
+              href=""
               class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
             >
               About Us
@@ -40,18 +40,10 @@
           </li>
           <li>
             <a
-              href="http://blog.creative-tim.com?ref=ns-footer-admin"
+              href="n"
               class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
             >
               Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/creativetimofficial/notus-svelte/blob/main/LICENSE.md?ref=ns-footer-admin"
-              class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
-            >
-              MIT License
             </a>
           </li>
         </ul>
