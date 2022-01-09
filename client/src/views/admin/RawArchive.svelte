9af8f1d3dd12319db6cc1263e418dde1cc99958c
Andrew <andrewm1246@gmail.com>
2022-01-09 13:04:29 -0500

    Make RawArchive load dynamically from JSON

--- client/src/views/admin/RawArchive.svelte
+++ client/src/views/admin/RawArchive.svelte
@@ -20,13 +20,24 @@
     $: peopleTypes = $peopleTypesStore;
     $: countries = $countriesStore;
 
+    let tableData = [];
+    $: tableData = [
+        organizations,
+        organizationTypes,
+        people,
+        peopleTypes,
+        countries,
+    ];
+
+    $: console.log(tableData);
+
     // Defines input buttons for HeaderStats
     let DataSettings;
     $: DataSettings = {
         // People Start
         People: {
             // People Inputs
-            Inputs: [
+            HeaderSearchInputs: [
                 {
                     type: "Text",
                     placeholder: "First Name",
@@ -89,6 +100,7 @@
             // People Cards End
             // People Table Begin
             Table: {
+                AwaitData: [people],
                 Headers: [
                     "First Name",
                     "Last Name",
@@ -121,6 +133,7 @@
             // People Table End
             // People CreationCard Begin
             CreationCard: {
+                AwaitData: [organizations, peopleTypes],
                 URL: "/admin/create-person",
                 RefreshURL: "/admin/people",
                 Title: "Create Person",
@@ -191,7 +204,7 @@
         // People Types Start
         PeopleTypes: {
             // People Types Inputs
-            Inputs: [
+            HeaderSearchInputs: [
                 {
                     type: "Text",
                     placeholder: "Person Type Name",
@@ -233,6 +246,7 @@
             // People Types Cards End
             // People Types Table Begin
             Table: {
+                AwaitData: [peopleTypes],
                 Headers: ["Person Type Name", "Acronyms"],
                 DBFieldNames: ["person_type_name", "person_type_acronyms"],
                 DeletionURL: "/admin/delete-person-type",
@@ -244,6 +258,7 @@
             // People Types Table End
             // People Types CreationCard Begin
             CreationCard: {
+                AwaitData: [peopleTypes],
                 URL: "/admin/create-person-type",
                 RefreshURL: "/admin/person-types",
                 Title: "Create Person Type",
@@ -274,7 +289,7 @@
         // Organizations Start
         Organizations: {
             // Organizations Inputs
-            Inputs: [
+            HeaderSearchInputs: [
                 {
                     type: "Text",
                     placeholder: "Name",
@@ -321,6 +336,7 @@
             // Organizations Cards End
             // Organizations Table Begin
             Table: {
+                AwaitData: [organizations],
                 Headers: [
                     "Name",
                     "Organization Type",
@@ -347,6 +363,7 @@
             // Organizations Table End
             // Organizations CreationCard Begin
             CreationCard: {
+                AwaitData: [organizations],
                 URL: "/admin/create-organization",
                 RefreshURL: "/admin/organizations",
                 Title: "Create Organization",
@@ -405,7 +422,7 @@
         // Organization Types Start
         OrganizationTypes: {
             // Organization Types Inputs
-            Inputs: [
+            HeaderSearchInputs: [
                 {
                     type: "Text",
                     placeholder: "Organization Type Name",
@@ -441,6 +458,7 @@
             // Organization Types Cards End
             // Organization Types Table Begin
             Table: {
+                AwaitData: [organizationTypes],
                 Headers: ["Organization Type Name", "Acronyms"],
                 DBFieldNames: ["organ_type_name", "organ_type_acronyms"],
                 DeletionURL: "/admin/delete-organization-type",
@@ -455,6 +473,7 @@
             // Organization Types Table End
             // Organization Types CreationCard Begin
             CreationCard: {
+                AwaitData: [organizationTypes],
                 URL: "/admin/create-organization-type",
                 RefreshURL: "/admin/organization-types",
                 Title: "Create Organization Type",
@@ -485,7 +504,7 @@
         // Countries Start
         Countries: {
             // Countries Inputs
-            Inputs: [
+            HeaderSearchInputs: [
                 {
                     type: "Text",
                     placeholder: "Name",
@@ -538,6 +557,7 @@
             // Countries Cards End
             // Countries Table Begin
             Table: {
+                AwaitData: [countries],
                 Headers: [
                     "Area",
                     "Capital",
@@ -574,7 +594,6 @@
         "People",
         "Countries",
         "Organizations",
-        "Maps",
         "People Types",
         "Organization Types",
     ];
@@ -584,246 +603,57 @@
 </script>
 
 <AdminNavbar bind:openTab {navItems} title={"Raw Archive"} />
-
-<!-- People -->
-<div class={navItems[openTab] === "People" ? "block" : "hidden"}>
-    <HeaderStats
-        id={"People"}
-        cards={DataSettings.People.Cards}
-        title={"People"}
-        titleFontSize={"text-4xl"}
-        inputs={DataSettings.People.Inputs}
-    />
-    <div class="block px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap ml-8">
-            <div
-                class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
-            >
-                {#await people}
-                    <p>Loading...</p>
-                {:then people}
-                    <CardTable
-                        color="dark"
-                        data={people}
-                        headers={DataSettings.People.Table.Headers}
-                        DBFieldNames={DataSettings.People.Table.DBFieldNames}
-                        title={DataSettings.People.Table.Title}
-                        DeletionURL={DataSettings.People.Table.DeletionURL}
-                        RefreshURL={DataSettings.People.Table.RefreshURL}
-                        UpdateURL={DataSettings.People.Table.UpdateURL}
-                        UpdateFormNames={DataSettings.People.Table
-                            .UpdateFormNames}
+{#if tableData.includes(undefined) !== true}
+    {#each Object.entries(DataSettings) as section}
+        <div
+            class={navItems[openTab].replace(" ", "") === section[0]
+                ? "block"
+                : "hidden"}
+        >
+            <HeaderStats
+                id={section[0]}
+                cards={section[1].Cards}
+                title={navItems[openTab]}
+                titleFontSize={"text-4xl"}
+                inputs={section[1].HeaderSearchInputs}
+            />
+            <div class="block px-4 md:px-10 mx-auto w-full m-12">
+                <div class="flex flex-wrap ml-8">
+                    <div
+                        class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
+                    >
+                        {#await section[1].Table.AwaitData[0]}
+                            <p>Loading...</p>
+                        {:then data}
+                            <CardTable
+                                color="dark"
+                                {data}
+                                headers={section[1].Table.Headers}
+                                DBFieldNames={section[1].Table.DBFieldNames}
+                                title={section[1].Table.Title}
+                                DeletionURL={section[1].Table.DeletionURL}
+                                RefreshURL={section[1].Table.RefreshURL}
+                                UpdateURL={section[1].Table.UpdateURL}
+                                UpdateFormNames={section[1].Table
+                                    .UpdateFormNames}
+                            />
+                        {:catch error}
+                            <p style="color: red">{error.message}</p>
+                        {/await}
+                    </div>
+                </div>
+            </div>
+            {#if section[1].CreationCard !== undefined}
+                {#if section[1].CreationCard.AwaitData.includes(undefined) !== true}
+                    <DataCreationCard
+                        flexdata={section[1].CreationCard.Flexdatalistdata}
+                        url={section[1].CreationCard.URL}
+                        title={section[1].CreationCard.Title}
+                        inputs={section[1].CreationCard.Inputs}
+                        refreshURL={section[1].CreationCard.RefreshURL}
                     />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-            </div>
+                {/if}
+            {/if}
         </div>
-    </div>
-    {#if organizations !== undefined && peopleTypes !== undefined}
-        <DataCreationCard
-            flexdata={DataSettings.People.CreationCard.Flexdatalistdata}
-            url={DataSettings.People.CreationCard.URL}
-            title={DataSettings.People.CreationCard.Title}
-            inputs={DataSettings.People.CreationCard.Inputs}
-            refreshURL={DataSettings.People.CreationCard.RefreshURL}
-        />
-    {/if}
-</div>
-
-<!-- People Types -->
-<div class={navItems[openTab] === "People Types" ? "block" : "hidden"}>
-    <HeaderStats
-        id={"PeopleTypes"}
-        title={"People Types"}
-        titleFontSize={"text-4xl"}
-        cards={DataSettings.PeopleTypes.Cards}
-        inputs={DataSettings.PeopleTypes.Inputs}
-    />
-    <div class="block px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap ml-8">
-            <div
-                class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
-            >
-                {#await peopleTypes}
-                    <p>Loading...</p>
-                {:then peopleTypes}
-                    <CardTable
-                        color="dark"
-                        data={peopleTypes}
-                        DBFieldNames={DataSettings.PeopleTypes.Table
-                            .DBFieldNames}
-                        headers={DataSettings.PeopleTypes.Table.Headers}
-                        title={DataSettings.PeopleTypes.Table.Title}
-                        DeletionURL={DataSettings.PeopleTypes.Table.DeletionURL}
-                        RefreshURL={DataSettings.PeopleTypes.Table.RefreshURL}
-                        UpdateURL={DataSettings.PeopleTypes.Table.UpdateURL}
-                        UpdateFormNames={DataSettings.PeopleTypes.Table
-                            .UpdateFormNames}
-                    />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-            </div>
-        </div>
-    </div>
-    {#if peopleTypes !== undefined}
-        <DataCreationCard
-            flexdata={DataSettings.PeopleTypes.CreationCard.Flexdatalistdata}
-            url={DataSettings.PeopleTypes.CreationCard.URL}
-            title={DataSettings.PeopleTypes.CreationCard.Title}
-            inputs={DataSettings.PeopleTypes.CreationCard.Inputs}
-            refreshURL={DataSettings.PeopleTypes.CreationCard.RefreshURL}
-        />
-    {/if}
-</div>
-
-<!-- Countries -->
-<div class={navItems[openTab] === "Countries" ? "block" : "hidden"}>
-    <HeaderStats
-        id={"Countries"}
-        title={"Countries"}
-        titleFontSize={"text-4xl"}
-        cards={DataSettings.Countries.Cards}
-        inputs={DataSettings.Countries.Inputs}
-    />
-    <div class="block px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap ml-8">
-            <div
-                class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
-            >
-                {#await countries}
-                    <p>Loading...</p>
-                {:then countries}
-                    <CardTable
-                        color="dark"
-                        data={countries}
-                        headers={DataSettings.Countries.Table.Headers}
-                        DBFieldNames={DataSettings.Countries.Table.DBFieldNames}
-                        title={DataSettings.Countries.Table.Title}
-                        DeletionURL={DataSettings.Countries.Table.DeletionURL}
-                        RefreshURL={DataSettings.Countries.Table.RefreshURL}
-                        Modification={DataSettings.Countries.Table.Modification}
-                    />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Organizations -->
-<div class={navItems[openTab] === "Organizations" ? "block" : "hidden"}>
-    <HeaderStats
-        id={"Organizations"}
-        title={"Organizations"}
-        titleFontSize={"text-4xl"}
-        cards={DataSettings.Organizations.Cards}
-        inputs={DataSettings.Organizations.Inputs}
-    />
-    <div class="block px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap ml-8">
-            <div
-                class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
-            >
-                {#await organizations}
-                    <p>Loading...</p>
-                {:then organizations}
-                    <CardTable
-                        color="dark"
-                        data={organizations}
-                        DBFieldNames={DataSettings.Organizations.Table
-                            .DBFieldNames}
-                        headers={DataSettings.Organizations.Table.Headers}
-                        title={DataSettings.Organizations.Table.Title}
-                        DeletionURL={DataSettings.Organizations.Table
-                            .DeletionURL}
-                        RefreshURL={DataSettings.Organizations.Table.RefreshURL}
-                        UpdateURL={DataSettings.Organizations.Table.UpdateURL}
-                        UpdateFormNames={DataSettings.Organizations.Table
-                            .UpdateFormNames}
-                    />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-            </div>
-        </div>
-    </div>
-    {#if organizations !== undefined}
-        <DataCreationCard
-            flexdata={DataSettings.Organizations.CreationCard.Flexdatalistdata}
-            url={DataSettings.Organizations.CreationCard.URL}
-            title={DataSettings.Organizations.CreationCard.Title}
-            inputs={DataSettings.Organizations.CreationCard.Inputs}
-            refreshURL={DataSettings.Organizations.CreationCard.RefreshURL}
-        />
-    {/if}
-</div>
-
-<!-- Organization Types -->
-<div class={navItems[openTab] === "Organization Types" ? "block" : "hidden"}>
-    <HeaderStats
-        id={"OrganizationTypes"}
-        title={"Organization Types"}
-        titleFontSize={"text-4xl"}
-        cards={DataSettings.OrganizationTypes.Cards}
-        inputs={DataSettings.OrganizationTypes.Inputs}
-    />
-    <div class="block px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap ml-8">
-            <div
-                class="w-full h-600-px bg-blueGray-700 mt-12 mb-12 flex justify-center items-center p-8"
-            >
-                {#await organizationTypes}
-                    <p>Loading...</p>
-                {:then organizationTypes}
-                    <CardTable
-                        color="dark"
-                        data={organizationTypes}
-                        DBFieldNames={DataSettings.OrganizationTypes.Table
-                            .DBFieldNames}
-                        headers={DataSettings.OrganizationTypes.Table.Headers}
-                        title={DataSettings.OrganizationTypes.Table.Title}
-                        DeletionURL={DataSettings.OrganizationTypes.Table
-                            .DeletionURL}
-                        RefreshURL={DataSettings.OrganizationTypes.Table
-                            .RefreshURL}
-                        UpdateURL={DataSettings.OrganizationTypes.Table
-                            .UpdateURL}
-                        UpdateFormNames={DataSettings.OrganizationTypes.Table
-                            .UpdateFormNames}
-                    />
-                {:catch error}
-                    <p style="color: red">{error.message}</p>
-                {/await}
-            </div>
-        </div>
-    </div>
-    {#if organizationTypes !== undefined}
-        <DataCreationCard
-            flexdata={DataSettings.OrganizationTypes.CreationCard
-                .Flexdatalistdata}
-            url={DataSettings.OrganizationTypes.CreationCard.URL}
-            title={DataSettings.OrganizationTypes.CreationCard.Title}
-            inputs={DataSettings.OrganizationTypes.CreationCard.Inputs}
-            refreshURL={DataSettings.OrganizationTypes.CreationCard.RefreshURL}
-        />
-    {/if}
-</div>
-
-<!-- Maps, Graphs, Charts -->
-<div class={navItems[openTab] === "Maps" ? "block" : "hidden"}>
-    <HeaderStats id={"Maps"} title={"Maps"} titleFontSize={"text-4xl"} />
-    <div class="px-4 md:px-10 mx-auto w-full m-12">
-        <div class="flex flex-wrap">
-            <div class="w-full h-500-px bg-blueGray-700 mt-12 mb-12">
-                <h2
-                    class="text-5xl font-normal leading-normal mt-0 mb-2 text-white"
-                >
-                    Test
-                </h2>
-            </div>
-        </div>
-    </div>
-</div>
+    {/each}
+{/if}
