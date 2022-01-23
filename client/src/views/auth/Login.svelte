14308d5653a569d1a6ddbaec454d696a567336ef
Andrew <andrewm1246@gmail.com>
2022-01-22 19:13:19 -0500

    Cleaned up registration and login routes

--- client/src/views/auth/Login.svelte
+++ client/src/views/auth/Login.svelte
@@ -1,9 +1,6 @@
 <script>
   import { link } from "svelte-routing";
 
-  // core components
-  const github = "../assets/img/github.svg";
-  const google = "../assets/img/google.svg";
   export let location;
 </script>
 
@@ -13,31 +10,9 @@
       <div
         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
-        <div class="rounded-t mb-0 px-6 py-6">
-          <div class="text-center mb-3">
-            <h6 class="text-blueGray-500 text-sm font-bold">Sign in with</h6>
-          </div>
-          <div class="btn-wrapper text-center">
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src={github} />
-              Github
-            </button>
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src={google} />
-              Google
-            </button>
-          </div>
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
-        </div>
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div class="text-blueGray-400 text-center mb-3 font-bold">
-            <small>Or sign in with credentials</small>
+          <div class="py-4 text-blueGray-400 text-center mb-3 font-bold">
+            <small>Sign in with credentials</small>
           </div>
           <form action="/auth/login" method="post">
             <div class="relative w-full mb-3">
@@ -88,7 +63,6 @@
               <button
                 class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                 type="submit"
-                on:click={submitLogin}
               >
                 Sign In
               </button>
--- client/src/views/auth/Register.svelte
+++ client/src/views/auth/Register.svelte
@@ -1,7 +1,4 @@
 <script>
-  // core components
-  const github = "../assets/img/github.svg";
-  const google = "../assets/img/google.svg";
   export let location;
 </script>
 
@@ -11,31 +8,9 @@
       <div
         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
-        <div class="rounded-t mb-0 px-6 py-6">
-          <div class="text-center mb-3">
-            <h6 class="text-blueGray-500 text-sm font-bold">Sign up with</h6>
-          </div>
-          <div class="btn-wrapper text-center">
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src={github} />
-              Github
-            </button>
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src={google} />
-              Google
-            </button>
-          </div>
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
-        </div>
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div class="text-blueGray-400 text-center mb-3 font-bold">
-            <small>Or sign up with credentials</small>
+          <div class="py-4 text-blueGray-400 text-center mb-3 font-bold">
+            <small>Sign up with credentials</small>
           </div>
           <form action="/auth/register-user" method="post">
             <div class="relative w-full mb-3">
