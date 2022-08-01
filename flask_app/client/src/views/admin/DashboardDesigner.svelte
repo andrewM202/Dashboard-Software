5ca4cb41a8ffa1a1e520e2ae78f1585b8ee9e3eb
Andrew <andrewm1246@gmail.com>
2022-07-31 22:45:45 -0400

    Fixed bug where new text would not be created where you initially right clicked, but where you clicked on the 'create text' button

--- flask_app/client/src/views/admin/DashboardDesigner.svelte
+++ flask_app/client/src/views/admin/DashboardDesigner.svelte
@@ -2,6 +2,7 @@
     let showActionBar = false;
     let parentOffset;
     let chartsCreated = 0;
+    let textCreated = 0;
 
     // onLoad contains event listeners needing to be attached
     // when #DashboardDesignerContainer is loaded
@@ -41,16 +42,16 @@
             }px; width: 500px; height: 500px;" class="absolute bg-blueGray-700 text-white">
                 <div class="chart-icon-container flex justify-between">
                     <i style="padding-left: 7px; padding-top: 7px;" class="fa fa-bars cursor-pointer chart-mover" aria-hidden="true"></i>
-                    <i style="padding-right: 7px; padding-top: 7px;" class="fa fa-cog cursor-pointer" aria-hidden="true"></i>
+                    <i style="padding-right: 7px; padding-top: 7px;" class="fa fa-cog cursor-pointer chart-settings" aria-hidden="true"></i>
                 </div>
                 <h1 class="text-4xl text-white text-center">Chart Title</h1>
                 <div
                     style="right: -4px; bottom: -4px; cursor: move;"
-                    class="bg-rose-400 h-4 w-4 absolute rounded-full px-2"
+                    class="resizer bg-rose-400 h-4 w-4 absolute rounded-full px-2"
                 ></div>
                 <div
                     style="left: -4px; bottom: -4px; cursor: move;"
-                    class="bg-rose-400 h-4 w-4 absolute rounded-full px-2"
+                    class="resizer bg-rose-400 h-4 w-4 absolute rounded-full px-2"
                 ></div>
             </div>`
         );
@@ -60,7 +61,7 @@
         let parent = j$(`#chart${chartsCreated}`);
         let minParentHeight = 250;
         let minParentWidth = 250;
-        j$(`#chart${chartsCreated} div`).mousedown(function (downEv) {
+        j$(`#chart${chartsCreated} div.resizer`).mousedown(function (downEv) {
             downEv.preventDefault();
             j$(window).mousemove(function (moveEv) {
                 let initialPosY = j$(downEv.target).offset().top;
@@ -128,6 +129,24 @@
             });
         });
 
+        // Event listener for chart settings
+        j$(`#chart${chartsCreated} i.chart-settings`).click(function () {
+            console.log("chart settings clicked");
+            j$(`#chart${chartsCreated}`).append(`
+            <div
+                id="DashboardDesignerActionBar"
+                class="absolute bg-orange-500 text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 min-w-48"
+            >
+                <a
+                    href="#pablo"
+                    class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-white"
+                >
+                    Create Chart
+                </a>
+            </div>
+            `);
+        });
+
         // Resize the dashboard chart container if the new chart we are creating
         // goes outside of the dashboard chart container
         if (
@@ -143,6 +162,22 @@
 
         // Increment the chartsCreated variable now that a new chart is created
         chartsCreated++;
+    }
+
+    // Function for creating text
+    function createText(event) {
+        // Create a text node where clicked
+        j$("#DashboardDesignerContainer").append(`
+            <h1 id="text${textCreated}" style="top: ${j$(
+            "#DashboardDesignerContainer > #DashboardDesignerActionBar"
+        ).css("top")}; left: ${j$(
+            "#DashboardDesignerContainer > #DashboardDesignerActionBar"
+        ).css("left")};"
+            class="absolute"
+            >Text
+            </h1>`);
+
+        textCreated++;
     }
 </script>
 
@@ -168,12 +203,19 @@
             href="#pablo"
             class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-white"
         >
-            Delete Chart
+            Delete Selected Charts
+        </a>
+        <a
+            href="#pablo"
+            class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-white"
+        >
+            Group Selected Charts
         </a>
         <div
             class="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25"
         />
         <a
+            on:click={createText}
             href="#pablo"
             class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-white"
         >
