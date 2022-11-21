6953686fecc866d44170588c3334605cb7e03b17
Andrew <andrewm1246@gmail.com>
2022-11-21 18:33:48 -0500

    Updated nginx and minified tailwind.css

--- flask_app/client/src/components/Navbars/AdminNavbar.svelte
+++ flask_app/client/src/components/Navbars/AdminNavbar.svelte
@@ -1,47 +1,47 @@
 <script>
-  // core components
-  export let navItems;
-  export let openTab;
-  export let title;
-  export let navBarBGColor;
+	// core components
+	export let navItems;
+	export let openTab;
+	export let title;
+	export let navBarBGColor;
 </script>
 
 <!-- Navbar -->
 <nav
-  style={navBarBGColor}
-  class="bg-red-500 block w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start items-center"
+	style={navBarBGColor}
+	class="bg-red-500 block w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start items-center"
 >
-  {#if title !== undefined}
-    <div class="md:block hidden md:pr-10 px-4 w-40">
-      <a
-        class="text-white min-w-full text-sm uppercase align-self-start hidden lg:inline-block font-semibold"
-        href="#pablo"
-        on:click={(e) => e.preventDefault()}
-      >
-        {title}
-      </a>
-    </div>
-  {/if}
-  <div
-    class="w-full mx-auto items-center flex justify-center flex-wrap md:flex-wrap py-4"
-  >
-    <!-- Brand -->
+	{#if title !== undefined}
+		<div class="md:block hidden md:pr-10 px-4 w-40">
+			<a
+				class="text-white min-w-full text-sm uppercase align-self-start hidden lg:inline-block font-semibold"
+				href="#pablo"
+				on:click={(e) => e.preventDefault()}
+			>
+				{title}
+			</a>
+		</div>
+	{/if}
+	<div
+		class="w-full mx-auto items-center flex justify-center flex-wrap md:flex-wrap py-4"
+	>
+		<!-- Brand -->
 
-    {#if navItems !== undefined}
-      {#each navItems as navItem, i}
-        <p
-          on:click={() => (openTab = i)}
-          style="margin: 5px; color: black; padding: 10px; border-radius: 5px;"
-          class="{openTab === i
-            ? 'bg-gray-200'
-            : 'bg-white'} truncate nav-item cursor-pointer text-sm uppercase text-center"
-        >
-          {navItem}
-        </p>
-      {/each}
-    {/if}
-    <!-- Form -->
-    <!-- <form
+		{#if navItems !== undefined}
+			{#each navItems as navItem, i}
+				<p
+					on:click={() => (openTab = i)}
+					style="margin: 5px; color: black; padding: 10px; border-radius: 5px;"
+					class="{openTab === i
+						? 'bg-gray-200'
+						: 'bg-white'} truncate nav-item cursor-pointer text-sm uppercase text-center"
+				>
+					{navItem}
+				</p>
+			{/each}
+		{/if}
+		<!-- Form -->
+		<!-- <form
       class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
     >
       <div class="relative flex w-full flex-wrap items-stretch">
@@ -57,41 +57,43 @@
         />
       </div>
     </form> -->
-  </div>
+	</div>
 </nav>
 
-{#if navItems.length === 1}
-  <style>
-  </style>
-{:else if navItems.length === 2}
-  <style>
-    @media (min-width: 1024px) {
-      p.nav-item {
-        width: calc(50% - 30px) !important;
-      }
-    }
-  </style>
-{:else if navItems.length === 3}
-  <style>
-    @media (min-width: 1024px) {
-      p.nav-item {
-        width: calc(33% - 30px) !important;
-      }
-    }
-  </style>
-{:else}
-  <style>
-    @media (min-width: 1024px) {
-      p.nav-item {
-        width: calc(25% - 30px) !important;
-      }
-    }
-  </style>
+{#if navItems !== undefined}
+	{#if navItems.length === 1}
+		<style>
+		</style>
+	{:else if navItems.length === 2}
+		<style>
+			@media (min-width: 1024px) {
+				p.nav-item {
+					width: calc(50% - 30px) !important;
+				}
+			}
+		</style>
+	{:else if navItems.length === 3}
+		<style>
+			@media (min-width: 1024px) {
+				p.nav-item {
+					width: calc(33% - 30px) !important;
+				}
+			}
+		</style>
+	{:else}
+		<style>
+			@media (min-width: 1024px) {
+				p.nav-item {
+					width: calc(25% - 30px) !important;
+				}
+			}
+		</style>
+	{/if}
 {/if}
 
 <!-- End Navbar -->
 <style>
-  p.nav-item {
-    width: calc(100% - 30px);
-  }
+	p.nav-item {
+		width: calc(100% - 30px);
+	}
 </style>
